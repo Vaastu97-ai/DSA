c4952a768da5715c4b2f9a9aef6b43e7f248e63c
Darshan K <darshanjanud4@gmail.com>
2026-02-22 18:53:17 +0530

    updated the test case for the method

--- Zero_to_end.py
+++ Zero_to_end.py
@@ -8,3 +8,10 @@ def Zero_to_end(arr):
             j += 1
         return arr
     
+arr = list(map(int , input("Enter the numbers:").split()))
+
+print(arr)
+
+print("Array after moving the zero to end:",Zero_to_end(arr))
+
+    
